dbc3185b373b09d57060657e572056fc1a08a1bf
davidmolina2810 <davidmolina2810@gmail.com>
2020-11-28 10:02:41 -0600

    Finished. Last test won't pass

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
 /* Your Code Here */
+const createEmployeeRecord = (arr) => {
+  const record = {
+    "firstName":arr[0],
+    "familyName":arr[1],
+    "title":arr[2],
+    "payPerHour":arr[3],
+    "timeInEvents":[],
+    "timeOutEvents":[]
+  }
+  return record
+}
+
+const createEmployeeRecords = (arr) => {
+  return arr.map(emp => createEmployeeRecord(emp))
+}
+
+function createTimeInEvent(dateTime) {
+  const sliceAt = dateTime.indexOf(" ")
+  const date = dateTime.slice(0, sliceAt)
+  const time = parseInt(dateTime.slice(sliceAt))
+  const newEvent = {
+    "type":"TimeIn",
+    "date":date,
+    "hour":time
+  }
+  this.timeInEvents.push(newEvent)
+  return this
+}
+
+function createTimeOutEvent(dateTime) {
+  const sliceAt = dateTime.indexOf(" ")
+  const date = dateTime.slice(0, sliceAt)
+  const time = parseInt(dateTime.slice(sliceAt))
+  const newEvent = {
+    "type":"TimeOut",
+    "date":date,
+    "hour":time
+  }
+  this.timeOutEvents.push(newEvent)
+  return this
+}
+
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(e => e.date === date).hour 
+  const timeOut = this.timeOutEvents.find(e => e.date === date).hour
+  return (timeOut - timeIn)/100
+}
+
+function wagesEarnedOnDate(date) {
+  const wage = this.payPerHour
+  const hoursToday = hoursWorkedOnDate.call(this, date)
+  return wage * hoursToday
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,4 +72,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+const findEmployeeByFirstName = (arr, name) => {
+  return arr.find(emp => emp.firstName === name)
+}
+
+function calculatePayroll(employeesArr) {
+  employeesArr.reduce((acc, emp) => {
+    return acc + allWagesFor.call(emp)
+  }, 0)
 }
